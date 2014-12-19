fe3ec22b5bb4eebef1b0a2663da009951dc580cc
Daniil Sorokin <magpie.danial@gmail.com>
2014-12-19 17:32:51 +0100

    Tests.

--- src/test/java/de/nlptools/languageid/tools/MathToolsTest.java
+++ src/test/java/de/nlptools/languageid/tools/MathToolsTest.java
@@ -1,6 +1,10 @@
-package de.nlptools.languageid;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.nlptools.languageid.tools;
 
-import de.nlptools.languageid.classifiers.NearestPrototypeClassifier;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -12,9 +16,9 @@ import static org.junit.Assert.*;
  *
  * @author dsorokin
  */
-public class NearestPrototypeClassifierTest {
+public class MathToolsTest {
     
-    public NearestPrototypeClassifierTest() {
+    public MathToolsTest() {
     }
     
     @BeforeClass
@@ -34,27 +38,27 @@ public class NearestPrototypeClassifierTest {
     }
 
     /**
-     * Test of cosine method, of class NearestPrototypeClassifier.
+     * Test of cosine method, of class MathTools.
      */
-    @org.junit.Test
+    @Test
     public void testCosine() {
         System.out.println("Test \"cosine\" method");
         double[] a = {0.0,0.0,1.0};
         double[] b = {0.0,0.0,-1.0};
         double expResult = -1.0;
-        double result = NearestPrototypeClassifier.cosine(a, b);
+        double result = MathTools.cosine(a, b);
         assertEquals(expResult, result, 0.00001);
 
         a = new double[]{0.0,2.0,0.0};
         b = new double[]{2.0,0.0,0.0};
         expResult = 0.0;
-        result = NearestPrototypeClassifier.cosine(a, b);
+        result = MathTools.cosine(a, b);
         assertEquals(expResult, result, 0.00001);
 
         a = new double[]{2.0,2.0,2.0};
         b = new double[]{1.0,0.0,0.5};
         expResult = 0.77459;
-        result = NearestPrototypeClassifier.cosine(a, b);
+        result = MathTools.cosine(a, b);
         assertEquals(expResult, result, 0.00001);
     }
     
